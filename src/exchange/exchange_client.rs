efed7c84c78d80c26bb7a96ed33054e27b18195a
royonia <roy@ergonia.io>
2025-12-04 23:07:57 +0800

    create_bulk_cancel & create_bulk_cancel_by_cloid

--- src/exchange/exchange_client.rs
+++ src/exchange/exchange_client.rs
@@ -198,7 +198,7 @@ impl ExchangeClient {
         self.post(action, signature, timestamp).await
     }
 
-    pub async fn create_bulk_order(
+    pub fn create_bulk_order(
         &self,
         orders: Vec<ClientOrderRequest>,
         wallet: Option<&LocalWallet>,
@@ -268,8 +268,46 @@ impl ExchangeClient {
         let action = serde_json::to_value(&action).map_err(|e| Error::JsonParse(e.to_string()))?;
         let is_mainnet = self.http_client.base_url == BaseUrl::Mainnet.get_url();
         let signature = sign_l1_action(wallet, connection_id, is_mainnet)?;
-
         self.post(action, signature, timestamp).await
+    }
+
+    pub fn create_bulk_cancel(
+        &self,
+        cancels: Vec<ClientCancelRequest>,
+        wallet: Option<&LocalWallet>,
+    ) -> Result<ExchangePayload> {
+        let wallet = wallet.unwrap_or(&self.wallet);
+        let timestamp = next_nonce();
+
+        let mut transformed_cancels = Vec::new();
+        for cancel in cancels.into_iter() {
+            let &asset = self
+                .coin_to_asset
+                .get(&cancel.asset)
+                .ok_or(Error::AssetNotFound)?;
+            transformed_cancels.push(CancelRequest {
+                asset,
+                oid: cancel.oid,
+            });
+        }
+
+        let action = Actions::Cancel(BulkCancel {
+            cancels: transformed_cancels,
+        });
+        let connection_id = action.hash(timestamp, self.vault_address)?;
+
+        let action = serde_json::to_value(&action).map_err(|e| Error::JsonParse(e.to_string()))?;
+        let is_mainnet = self.http_client.base_url == BaseUrl::Mainnet.get_url();
+        let signature = sign_l1_action(wallet, connection_id, is_mainnet)?;
+        let nonce = timestamp;
+
+        let exchange_payload = ExchangePayload {
+            action,
+            signature,
+            nonce,
+            vault_address: self.vault_address,
+        };
+        Ok(exchange_payload)
     }
 
     pub async fn cancel_by_cloid(
@@ -310,6 +348,45 @@ impl ExchangeClient {
         let signature = sign_l1_action(wallet, connection_id, is_mainnet)?;
 
         self.post(action, signature, timestamp).await
+    }
+
+    pub fn create_bulk_cancel_by_cloid(
+        &self,
+        cancels: Vec<ClientCancelRequestCloid>,
+        wallet: Option<&LocalWallet>,
+    ) -> Result<ExchangePayload> {
+        let wallet = wallet.unwrap_or(&self.wallet);
+        let timestamp = next_nonce();
+
+        let mut transformed_cancels: Vec<CancelRequestCloid> = Vec::new();
+        for cancel in cancels.into_iter() {
+            let &asset = self
+                .coin_to_asset
+                .get(&cancel.asset)
+                .ok_or(Error::AssetNotFound)?;
+            transformed_cancels.push(CancelRequestCloid {
+                asset,
+                cloid: uuid_to_hex_string(cancel.cloid),
+            });
+        }
+
+        let action = Actions::CancelByCloid(BulkCancelCloid {
+            cancels: transformed_cancels,
+        });
+
+        let connection_id = action.hash(timestamp, self.vault_address)?;
+        let action = serde_json::to_value(&action).map_err(|e| Error::JsonParse(e.to_string()))?;
+        let is_mainnet = self.http_client.base_url == BaseUrl::Mainnet.get_url();
+        let signature = sign_l1_action(wallet, connection_id, is_mainnet)?;
+        let nonce = timestamp;
+
+        let exchange_payload = ExchangePayload {
+            action,
+            signature,
+            nonce,
+            vault_address: self.vault_address,
+        };
+        Ok(exchange_payload)
     }
 
     pub async fn update_leverage(
