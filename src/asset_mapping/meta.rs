8dcab4004df255f4c2b3e7e40ea0bed4d144e534
Roy Tang (Contractor) <rtang@drwholdings.com>
2026-03-20 18:27:24 +0800

    feat: add self-contained meta structs for asset_mapping

---
+++ src/asset_mapping/meta.rs
@@ -0,0 +1,40 @@
+use serde::Deserialize;
+
+#[derive(Deserialize, Debug, Clone)]
+pub struct Meta {
+    pub universe: Vec<AssetMeta>,
+}
+
+#[derive(Deserialize, Debug, Clone)]
+#[serde(rename_all = "camelCase")]
+pub struct AssetMeta {
+    pub name: String,
+}
+
+#[derive(Deserialize, Debug, Clone)]
+#[serde(rename_all = "camelCase")]
+pub struct SpotMeta {
+    pub universe: Vec<SpotUniverse>,
+    pub tokens: Vec<SpotToken>,
+}
+
+#[derive(Deserialize, Debug, Clone)]
+#[serde(rename_all = "camelCase")]
+pub struct SpotUniverse {
+    pub tokens: [u32; 2],
+    pub name: String,
+    pub index: u32,
+}
+
+#[derive(Deserialize, Debug, Clone)]
+#[serde(rename_all = "camelCase")]
+pub struct SpotToken {
+    pub name: String,
+    pub index: u32,
+}
+
+#[derive(Deserialize, Debug, Clone)]
+#[serde(rename_all = "camelCase")]
+pub struct PerpDexMeta {
+    pub name: String,
+}
--- src/asset_mapping/mod.rs
+++ src/asset_mapping/mod.rs
@@ -1,3 +1,5 @@
 mod error;
+mod meta;
 
 pub use error::AssetMappingError;
+pub use meta::{AssetMeta, Meta, PerpDexMeta, SpotMeta, SpotToken, SpotUniverse};
