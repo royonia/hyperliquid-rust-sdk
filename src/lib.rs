172ddf2f91db6efd346745fb85d1f566aed014bc
Roy Tang (Contractor) <rtang@drwholdings.com>
2026-03-20 18:27:09 +0800

    feat: add asset_mapping module with error type

--- src/lib.rs
+++ src/lib.rs
@@ -10,6 +10,7 @@ mod proxy_digest;
 mod req;
 mod signature;
 mod ws;
+pub mod asset_mapping;
 pub use consts::{EPSILON, LOCAL_API_URL, MAINNET_API_URL, TESTNET_API_URL};
 pub use errors::Error;
 pub use exchange::*;
